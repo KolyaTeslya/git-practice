ccb403267f508a56d65891bccb69afc6a38c9511
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-09-02 10:49:35 +0300

    add two functions for ASC/DESC sorts

---
+++ ASC_and_DESC _functions.php
@@ -0,0 +1,55 @@
+<?php
+
+
+function AscSort($coll)
+{
+    $size = count($coll);
+
+    do {
+
+        $swapped = false;
+
+        for ($i = 0; $i < $size - 1; $i++) {
+            if ($coll[$i] > $coll[$i + 1]) {
+                $temp = $coll[$i];
+                $coll[$i] = $coll[$i + 1];
+                $coll[$i + 1] = $temp;
+                $swapped = true;
+            }
+        }
+        $size--;
+    } while ($swapped); 
+
+    return $coll;
+}
+
+print_r(AscSort([7, 2, 9, 10, -2, 0]));
+
+
+function DescSort($coll)
+{
+    $size = count($coll);
+
+    do {
+
+        $swapped = false;
+
+        for ($i = 0; $i < $size - 1; $i++) {
+            if ($coll[$i] < $coll[$i + 1]) {
+                $temp = $coll[$i];
+                $coll[$i] = $coll[$i + 1];
+                $coll[$i + 1] = $temp;
+                $swapped = true;
+            }
+        }
+        $size--;
+    } while ($swapped); 
+
+    return $coll;
+}
+
+print_r(DescSort([7, 2, 9, 10, -2, 0]));
+
+?>
+
+
